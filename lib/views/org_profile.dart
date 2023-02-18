87ac01d46ee7a9f0e68e584c602a50d0f82f0348
PawanPatil19 <pkp@Pawans-MacBook-Air.local>
2023-02-19 07:23:46 +0800

    A

--- lib/views/org_profile.dart
+++ lib/views/org_profile.dart
@@ -1,146 +1,46 @@
 import 'package:flutter/material.dart';
 import '../constants/constants.dart';
+import 'package:textfield_tags/textfield_tags.dart';
 
 // create a stateless widget
 
-class OrgProfile extends StatelessWidget {
+class OrgProfile extends StatefulWidget {
   const OrgProfile({Key? key}) : super(key: key);
+
+  @override
+  State<OrgProfile> createState() => _OrgProfileState();
+}
+
+class _OrgProfileState extends State<OrgProfile> {
+  double _distanceToField = 0;
+  TextfieldTagsController _controller = TextfieldTagsController();
+
+    @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _distanceToField = MediaQuery.of(context).size.width;
+  }
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    final tagWidth = width * 0.2;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
         leadingWidth: 0,
-        title: const Text('HelpingU', style: TextStyle(fontFamily: "Poppins", color: kTextColor, fontSize: 30,  fontWeight: FontWeight.bold),),
+        title: const Text('HelpinU', style: TextStyle(fontFamily: "Poppins", color: kTextColor, fontSize: 30,  fontWeight: FontWeight.bold),),
         actions: <Widget>[
         //add text
-        Icon(
-          Icons.person_outline_rounded,
-          color: kTextColor,
-          size: 25,),
-
-        const Padding(padding: EdgeInsets.only(right: 20)),
-      ],
-        
-      ),
-
-      body: Column(
-        //crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Image(
-            image: const AssetImage('/images/company.png'),
-            height: MediaQuery.of(context).size.height * 0.3,
-          ),
-
-          const SizedBox(height: 20),
-
-          Text(
-            'Company Name',
-            style: TextStyle(
-              fontSize: 30,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.center,
-          ),
-
-          const SizedBox(height: 10),
-
-          Container(
-            
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: kSecondaryColor,
-                width: 2,
-              ),
-            
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(30),
-            ),
-            child: Text(
-              'Tag', 
-              style: const TextStyle(
-                fontSize: 12,
-                fontFamily: 'Poppins',
-              ),
-            ),
-          ),
-
-          const SizedBox(height: 10),
-
-          // create a container for the description
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            
-            child: Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nisl nisl aliquam nunc, eget aliquam nisl nisl eget nisl. Donec auctor, nisl eget ultricies lacinia, nisl nisl aliquam nunc, eget aliquam nisl nisl eget nisl.',
-              style: TextStyle(
-                fontSize: 15,
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.bold,
-                color: Colors.grey
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-
-          const SizedBox(height: 20),
-
-        //create a container for email
         Container(
-          margin: const EdgeInsets.symmetric(horizontal: 40),
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: Colors.grey,
-              width: 2,
-            ),
-          ),
-          child: Text(
-              "Email",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontFamily: 'Poppins',
-              ),
-          ),
-        ),
-
-        const SizedBox(height: 10),
-
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 40),
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: Colors.grey,
-              width: 2,
-            ),
-          ),
-          child: Text(
-              "Phone",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontFamily: 'Poppins',
-              ),
-          ),
-        ),
-        
-        const SizedBox(height: 20),
-          
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 40),
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
           decoration: BoxDecoration(
             color: kSecondaryColor,
             borderRadius: BorderRadius.circular(10),
@@ -159,12 +59,278 @@ class OrgProfile extends StatelessWidget {
             ),
           ),
         ),
-          
+
+        const Padding(padding: EdgeInsets.only(right: 20)),
+      ],
         
+      ),
 
-              
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 10),
+          Image(
+            image: const AssetImage('/images/company.png'),
+            height: MediaQuery.of(context).size.height * 0.2,
+          ),
 
+          const SizedBox(height: 20),
 
+          //create a textfield for the name
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 40),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            child : Row(
+              children: [
+                const Text(
+                  'Name:',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.bold,
+                    
+                  ),
+                ),
+                const SizedBox(width: 20),
+                SizedBox(
+                  width: width * 0.7,
+                  child: TextField(
+                  controller: TextEditingController()..text = "Name of organization",
+                  keyboardType: TextInputType.name,
+                  onChanged: (value) {
+                  },
+                  decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                      
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 2, color: Colors.grey)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 2, color: kSecondaryColor))),
+                          ),
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 20),
+
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 40),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            child : Row(
+              children: [
+                const Text(
+                  'Email:',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.bold,
+                    
+                  ),
+                ),
+                const SizedBox(width: 20),
+                SizedBox(
+                  width: width * 0.7,
+                  child: TextField(
+                  controller: TextEditingController()..text = "abc@gmail.com",
+                  // make it ineditable
+                  enabled: false,
+                  keyboardType: TextInputType.name,
+                  onChanged: (value) {
+                  },
+                  decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                      
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 2, color: Colors.grey)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 2, color: kSecondaryColor))),
+                          ),
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 20),
+
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 40),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            child : Row(
+              children: [
+                const Text(
+                  'About:',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.bold,
+                    
+                  ),
+                ),
+                const SizedBox(width: 20),
+                SizedBox(
+                  width: width * 0.7,
+                  child: TextField(
+                  maxLines: 4,
+
+                  controller: TextEditingController()..text = "Description of organization",
+                  
+                  keyboardType: TextInputType.name,
+                  onChanged: (value) {
+                  },
+                  decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 2, color: Colors.grey)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 2, color: kSecondaryColor))),
+                          ),
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 20),
+
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 40),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            child: Row(
+              children: [
+                const Text(
+                    'Tags:',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.bold,
+                      
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                SizedBox(
+                  width: width * 0.7,
+                  child: TextFieldTags(
+                      textfieldTagsController: _controller,
+                      initialTags: const [
+                        'pick',
+                      ],
+                      textSeparators: const [' ', ','],
+                      letterCase: LetterCase.normal,
+                      validator: (String tag) {
+                        
+                      },
+                      inputfieldBuilder:
+                          (context, tec, fn, error, onChanged, onSubmitted) {
+                        return ((context, sc, tags, onTagDelete) {
+                          return Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: TextField(
+                              controller: tec,
+                              focusNode: fn,
+                              decoration: InputDecoration(
+                                isDense: true,
+                                border: const OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: kSecondaryColor,
+                                    width: 3.0,
+                                  ),
+                                ),
+                                focusedBorder: const OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: kSecondaryColor,
+                                    width: 3.0,
+                                  ),
+                                ),
+                        
+                                hintText: _controller.hasTags ? '' : "Enter tag...",
+                                errorText: error,
+                                prefixIconConstraints:
+                                    BoxConstraints(maxWidth: _distanceToField * 0.7),
+                                prefixIcon: tags.isNotEmpty
+                                    ? SingleChildScrollView(
+                                        controller: sc,
+                                        scrollDirection: Axis.horizontal,
+                                        child: Row(
+                                            children: tags.map((String tag) {
+                                          return Container(
+                                            decoration: const BoxDecoration(
+                                              borderRadius: BorderRadius.all(
+                                                Radius.circular(20.0),
+                                              ),
+                                              color: kSecondaryColor,
+                                            ),
+                                            margin: const EdgeInsets.symmetric(
+                                                horizontal: 5.0),
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 10.0, vertical: 5.0),
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceBetween,
+                                              children: [
+                                                InkWell(
+                                                  child: Text(
+                                                    '$tag',
+                                                    style: const TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                  onTap: () {
+                                                    print("$tag selected");
+                                                  },
+                                                ),
+                                                const SizedBox(width: 4.0),
+                                                InkWell(
+                                                  child: const Icon(
+                                                    Icons.cancel,
+                                                    size: 14.0,
+                                                    color: Color.fromARGB(
+                                                        255, 233, 233, 233),
+                                                  ),
+                                                  onTap: () {
+                                                    onTagDelete(tag);
+                                                  },
+                                                )
+                                              ],
+                                            ),
+                                          );
+                                        }).toList()),
+                                      )
+                                    : null,
+                              ),
+                              onChanged: onChanged,
+                              onSubmitted: onSubmitted,
+                            ),
+                          );
+                        });
+                      },
+                    ),
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 20),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 40),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            decoration: BoxDecoration(
+              color: kSecondaryColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: TextButton(
+              onPressed: () {
+                
+              },
+              child: const Text(
+                "Submit",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontFamily: 'Poppins',
+                ),
+              ),
+            ),
+          ),
 
         ],
         
